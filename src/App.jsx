00ddacca7184ab0b61328b433930adf25b556402
emiliaaslund <emilia.aslund@cmeducations.se>
2022-08-24 14:48:02 +0200

    håller tummarna

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const oldURL = "http://localhost:5000";
-// const newURL = "https://realtimechat-backend.herokuapp.com/";
-const socket = io(oldURL);
+// const oldURL = "http://localhost:5000";
+const newURL = "https://realtimechat-backend.herokuapp.com/";
+const socket = io(newURL);
 
 function App() {
   const [message, setMessage] = useState("");
